03a427d09385683b9a99f17946131965cefff91f
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-12 15:55:17 +0800

    feat: add aria

--- src/views/GameContent.vue
+++ src/views/GameContent.vue
@@ -2,26 +2,27 @@
 import { useStorage } from '@vueuse/core'
 
 const bgImage = useStorage('bgImage', 1, localStorage) || ref(1)
-
 </script>
 
 <template>
-  <div w-full h-full flex-center bg-center bg-cover bg-no-repeat :class="`bg-${bgImage}`">
-    <div text-3xl> Game Content</div>
+  <div w-full h-full flex-center bg-center bg-cover bg-no-repeat :style="{backgroundImage: `url(src/assets/bg-${bgImage}.jpg)`}">
+    <div h-full grid="~ rows-3">
+      <div h-full w-screen bg-red grid="~ cols-3">
+        <div w-full bg-gray:80 flex-center>记牌器区域</div>
+        <div w-full bg-gray:50 flex-center>手牌区域</div>
+        <div w-full bg-gray:80 flex-center>局数信息</div>
+      </div>
+      <div h-full w-screen bg-blue grid="~ cols-3">
+        <div w-full bg-gray:80 flex-center>空</div>
+        <div w-full bg-gray:50 flex-center>检查局域</div>
+        <div w-full bg-gray:80 flex-center>弃牌区域</div>
+      </div>
+      <div h-full w-screen bg-green grid="~ cols-3">
+        <div w-full bg-gray:80 flex-center>记牌器区域</div>
+        <div w-full bg-gray:50 flex-center>手牌区域</div>
+        <div w-full bg-gray:80 flex-center>局数信息</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-.bg-1 {
-  background-image: url(../assets/bg-1.jpg);
-}
-.bg-2 {
-  background-image: url(../assets/bg-2.jpg);
-}
-.bg-3 {
-  background-image: url(../assets/bg-3.jpg);
-}
-.bg-4 {
-  background-image: url(../assets/bg-4.jpg);
-}
-</style>
