ac7f055ef6c799a68df236053faa4754beb0fb21
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-12 17:49:29 +0800

    fix: change aria

--- src/views/GameContent.vue
+++ src/views/GameContent.vue
@@ -8,10 +8,10 @@ const bgImage = useStorage('bgImage', 1, localStorage) || ref(1)
 <template>
   <div w-full h-full flex-center bg-center bg-cover bg-no-repeat :style="{backgroundImage: `url(src/assets/bg-${bgImage}.jpg)`}">
     <div h-full grid="~ rows-4">
-      <div h-full w-screen bg-red grid="~ cols-3">
-        <div w-full bg-gray:80 flex-center>记牌器区域</div>
-        <div w-full bg-gray:50 flex-center>手牌区域</div>
-        <div w-full bg-gray:80 flex-center>局数信息</div>
+      <div h-full w-screen bg-red grid="~ cols-5">
+        <!-- <div w-full bg-gray:80 flex-center>记牌器区域</div> -->
+        <div col-span-3 w-full bg-gray:50 flex-center>手牌区域</div>
+        <div col-span-2 w-full bg-gray:80 flex-center>局数信息</div>
       </div>
       <div h-full w-screen bg-blue grid="~ cols-3">
         <div w-full bg-gray:80 flex-center>空</div>
@@ -23,10 +23,10 @@ const bgImage = useStorage('bgImage', 1, localStorage) || ref(1)
         <div w-full bg-gray:50 flex-center>检查局域</div>
         <div w-full bg-gray:80 flex-center>弃牌区域</div>
       </div>
-      <div h-full w-screen bg-green grid="~ cols-3">
-        <div w-full bg-gray:80 flex-center>记牌器区域</div>
-        <div w-full bg-gray:50 flex-center>手牌区域</div>
-        <div w-full bg-gray:80 flex-center>
+      <div h-full w-screen bg-green grid="~ cols-5">
+        <!-- <div w-full bg-gray:80 flex-center>记牌器区域</div> -->
+        <div col-span-3 w-full bg-gray:50 flex-center>手牌区域</div>
+        <div col-span-2 w-full bg-gray:80 flex-center>
           <GameInformation />
         </div>
       </div>
--- src/views/GameInformation.vue
+++ src/views/GameInformation.vue
@@ -16,14 +16,14 @@ const slaveScore = 21 - emperorScore
     <tr>
       <td>皇帝</td>
       <td v-for="v in emperorItems">
-        <div w-16px h-16px border-rd-8px :class="v ? 'bg-green' : 'bg-red'"></div>
+        <div w-20px h-20px border-rd-10px :class="v ? 'bg-green' : 'bg-red'"></div>
       </td>
       <td>{{ emperorScore }}</td>
     </tr>
     <tr>
       <td>奴隶</td>
       <td v-for="v in slaveItems">
-        <div w-16px h-16px border-rd-8px :class="v ? 'bg-red' : 'bg-green'"></div>  
+        <div w-20px h-20px border-rd-10px :class="v ? 'bg-red' : 'bg-green'"></div>  
       </td>
       <td>{{ slaveScore }}</td>
     </tr>
@@ -38,7 +38,7 @@ td {
 } 
 
 table tr td {
-  --uno: w-22px h-22px text-center line-height: 22px;
+  --uno: w-26px h-26px text-center;
 }
 
 tr td:nth-child(1),
