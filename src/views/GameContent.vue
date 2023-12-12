e0557ae6497f26c0fc83a0b205cec824ad488099
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-12 15:58:02 +0800

    fix: change aria

--- src/views/GameContent.vue
+++ src/views/GameContent.vue
@@ -6,13 +6,18 @@ const bgImage = useStorage('bgImage', 1, localStorage) || ref(1)
 
 <template>
   <div w-full h-full flex-center bg-center bg-cover bg-no-repeat :style="{backgroundImage: `url(src/assets/bg-${bgImage}.jpg)`}">
-    <div h-full grid="~ rows-3">
+    <div h-full grid="~ rows-4">
       <div h-full w-screen bg-red grid="~ cols-3">
         <div w-full bg-gray:80 flex-center>记牌器区域</div>
         <div w-full bg-gray:50 flex-center>手牌区域</div>
         <div w-full bg-gray:80 flex-center>局数信息</div>
       </div>
       <div h-full w-screen bg-blue grid="~ cols-3">
+        <div w-full bg-gray:80 flex-center>空</div>
+        <div w-full bg-gray:50 flex-center>检查局域</div>
+        <div w-full bg-gray:80 flex-center>弃牌区域</div>
+      </div>
+      <div h-full w-screen bg-yellow grid="~ cols-3">
         <div w-full bg-gray:80 flex-center>空</div>
         <div w-full bg-gray:50 flex-center>检查局域</div>
         <div w-full bg-gray:80 flex-center>弃牌区域</div>
@@ -25,4 +30,3 @@ const bgImage = useStorage('bgImage', 1, localStorage) || ref(1)
     </div>
   </div>
 </template>
-
