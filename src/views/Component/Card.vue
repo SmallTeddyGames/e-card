120a7a73dd5476d1301f25fa11c800b88a230569
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-13 17:28:09 +0800

    feat: add card animation

--- src/views/Component/Card.vue
+++ src/views/Component/Card.vue
@@ -8,16 +8,58 @@ const props = defineProps({
   }
 })
 
-const emperorCardItems = ref([ 'emperor', 'citizen', 'citizen', 'citizen', 'citizen'])
-const slaveCardItems = ref([ 'slave', 'citizen', 'citizen', 'citizen', 'citizen'])
+type CardItem = {
+  role: string
+  img: string
+  isClick: boolean
+}
+
+const emperorCardItems = ref([
+  { role: 'emperor', img: 'emperor.jpg', isClick: false, sort: 1 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 2 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 3 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 4 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 5 }
+])
+const slaveCardItems = ref([
+  { role: 'slave',   img: 'slave.jpg',   isClick: false, sort: 1 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 2 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 3 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 4 },
+  { role: 'citizen', img: 'citizen.jpg', isClick: false, sort: 5 }
+])
 
 const cardItems = computed(() => {
   return props.role === 'emperor' ? emperorCardItems.value : slaveCardItems.value
 })
+
+const handleCardClick = (cardInfo: CardItem) => {
+  cardItems.value.map(v => v.isClick = false)
+  cardInfo.isClick = true
+}
 </script>
 
 <template>
   <div grid="~ cols-5 gap-5">
-    <img cursor-pointer w-120px v-for="img in cardItems" :src="getAssetsFile(`${img}.jpg`)">
+    <img
+      w-120px
+      cursor-pointer
+      v-for="cardItem in cardItems"
+      :class="cardItem.isClick ? 'clicked' : ''"
+      :alt="cardItem.role"
+      :src="getAssetsFile(cardItem.img)"
+      @click="handleCardClick(cardItem)"
+    />
   </div>
 </template>
+
+<style>
+img {
+  position: relative;
+  transition: top 0.3s ease;
+}
+
+.clicked {
+  top: -20px;
+}
+</style>
