fbb0aa6b16fc955d0b3ef1b25d1878fdc83ed530
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-13 14:08:39 +0800

    feat: add `Card`

---
+++ src/views/Component/Card.vue
@@ -0,0 +1,19 @@
+<script lang='ts' setup>
+const props = defineProps({
+  role: {
+    type: String,
+    default: 'emperor',
+  }
+})
+</script>
+
+<template>
+  <div grid="~ cols-5 gap-5">
+    <img w-120px v-show="props.role === 'emperor'" src="../../assets/emperor.jpg" alt="emperor">
+    <img w-120px v-show="props.role === 'slave'" src="../../assets/slave.jpg" alt="slave">
+    <img w-120px src="../../assets/citizen.jpg" alt="">
+    <img w-120px src="../../assets/citizen.jpg" alt="">
+    <img w-120px src="../../assets/citizen.jpg" alt="">
+    <img w-120px src="../../assets/citizen.jpg" alt="">
+  </div>
+</template>
--- src/views/Game/GameMain.vue
+++ src/views/Game/GameMain.vue
@@ -1,13 +1,18 @@
 <script lang='ts' setup>
 import GameInformation from '../Component/GameInformation.vue';
+import Card from '../Component/Card.vue'
 </script>
 
 <template>
   <div h-full grid="~ rows-4">
       <div h-full w-screen bg-red grid="~ cols-5">
         <!-- <div w-full bg-gray:80 flex-center>记牌器区域</div> -->
-        <div col-span-3 w-full bg-gray:50 flex-center>手牌区域</div>
-        <div col-span-2 w-full bg-gray:80 flex-center>局数信息</div>
+        <div col-span-3 w-full bg-gray:50 flex-center>
+          <Card role="slave" />
+        </div>
+        <div col-span-2 w-full bg-gray:80 flex-center>
+          <GameInformation />
+        </div>
       </div>
       <div h-full w-screen bg-blue grid="~ cols-3">
         <div w-full bg-gray:80 flex-center>空</div>
@@ -21,7 +26,9 @@ import GameInformation from '../Component/GameInformation.vue';
       </div>
       <div h-full w-screen bg-green grid="~ cols-5">
         <!-- <div w-full bg-gray:80 flex-center>记牌器区域</div> -->
-        <div col-span-3 w-full bg-gray:50 flex-center>手牌区域</div>
+        <div col-span-3 w-full bg-gray:50 flex-center>
+          <Card role="emperor" />
+        </div>
         <div col-span-2 w-full bg-gray:80 flex-center>
           <GameInformation />
         </div>
