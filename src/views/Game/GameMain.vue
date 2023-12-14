634f8f2a46846dd26d0a1a4c4228aedba392f1d5
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-14 15:28:27 +0800

    fix: change bg-image import method

--- src/views/Game/GameMain.vue
+++ src/views/Game/GameMain.vue
@@ -7,7 +7,7 @@ import StartInfo from '../Component/StartInfo.vue'
 <template>
   <StartInfo />
   <div h-full grid="~ rows-4">
-      <div h-full w-screen bg-red grid="~ cols-5">
+      <div h-full w-screen grid="~ cols-5">
         <!-- <div w-full bg-gray:80 flex-center>记牌器区域</div> -->
         <div col-span-3 w-full bg-gray:50 flex-center>
           <Card role="slave" />
@@ -16,17 +16,17 @@ import StartInfo from '../Component/StartInfo.vue'
           <GameInformation />
         </div>
       </div>
-      <div h-full w-screen bg-blue grid="~ cols-3">
-        <div w-full bg-gray:80 flex-center>空</div>
+      <div h-full w-screen grid="~ cols-3">
+        <div w-full bg-gray:50 flex-center></div>
         <div w-full bg-gray:50 flex-center>检查局域</div>
         <div w-full bg-gray:80 flex-center>弃牌区域</div>
       </div>
-      <div h-full w-screen bg-yellow grid="~ cols-3">
-        <div w-full bg-gray:80 flex-center>空</div>
+      <div h-full w-screen grid="~ cols-3">
+        <div w-full bg-gray:50 flex-center></div>
         <div w-full bg-gray:50 flex-center>检查局域</div>
         <div w-full bg-gray:80 flex-center>弃牌区域</div>
       </div>
-      <div h-full w-screen bg-green grid="~ cols-5">
+      <div h-full w-screen grid="~ cols-5">
         <!-- <div w-full bg-gray:80 flex-center>记牌器区域</div> -->
         <div col-span-3 w-full bg-gray:50 flex-center>
           <Card role="emperor" />
