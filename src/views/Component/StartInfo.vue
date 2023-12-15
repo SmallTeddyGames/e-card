132dab497cc09aa769add0692617e2d66ffe5287
qingjiuge <1585744557@qq.com>
2023-12-15 19:50:15 +0800

    添加开始按钮

--- src/views/Component/StartInfo.vue
+++ src/views/Component/StartInfo.vue
@@ -1,16 +1,23 @@
 <script lang='ts' setup>
-const showGameInfo = ref(true)
-
-onMounted(() => {
+const showGameInfo = ref(false);
+const show=ref(true)
+const startGame=()=>{
+  showGameInfo.value = true;
   setTimeout(() => {
-    showGameInfo.value = false
+    show.value=false
   }, 3000)
+}
+onMounted(() => {
+ 
 })
 </script>
 
 <template>
-  <div v-if="showGameInfo" flex-center flex-col h-screen w-screen relative font-size-40px bg-gray:50>
+  <div v-if="show" flex-center flex-col h-screen w-screen relative font-size-40px bg-gray:50>
+    <div v-if="!showGameInfo"><button @click="startGame">开始</button></div>
+   <div v-if="showGameInfo">
     <div w-160px h-56px flex-center rd-8px gap-5>第 <span text-green> X </span> 局</div>
     <div w-240px h-56px flex-center rd-8px>角色 ： <span text-red>国王</span> </div>
+   </div>
   </div>
 </template>
