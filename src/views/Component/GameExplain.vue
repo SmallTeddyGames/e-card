b52dd8727c938b9a4c9c1595b0e668890e000edd
SmallTeddy <zhangzeming1212@gmail.com>
2023-12-22 10:31:04 +0800

    fix: game plain

--- src/views/Component/GameExplain.vue
+++ src/views/Component/GameExplain.vue
@@ -18,17 +18,7 @@ const emits = defineEmits(["close"]);
         <p>赢则积1分，数则积0分，先赢到11分则游戏结束</p>
       </div>
       <button class="btn" @click="emits('close')">
-        <svg
-          height="16"
-          width="16"
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          viewBox="0 0 1024 1024"
-        >
-          <path
-            d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"
-          ></path>
-        </svg>
+        <div class="i-material-symbols:arrow-back-rounded icon"></div>
         <span>Back</span>
       </button>
     </div>
@@ -37,19 +27,22 @@ const emits = defineEmits(["close"]);
 <style scoped>
 /* 字体太大了，屏幕显示不完全，修改 */
 .game-explain {
-  padding: 20px;
   box-sizing: border-box;
 }
-p {
-  font-size: 2rem;
-  line-height: 3rem;
-}
+
 .card {
-  padding: 15px;
+  padding: 30px;
   border-radius: 10px;
   box-sizing: border-box;
   opacity: 0.8;
 }
+
+.card p {
+  font-size: 2rem;
+  line-height: 3rem;
+  padding: 0 15px;
+}
+
 .btn {
   font-size: 2rem;
   padding: 0 20px;
@@ -57,22 +50,12 @@ p {
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 10px;
   border-radius: 3px;
   letter-spacing: 1px;
   transition: all 0.2s linear;
   cursor: pointer;
   border: none;
-}
-.btn > svg {
-  margin-right: 5px;
-  margin-left: 5px;
-  font-size: 20px;
-  transition: all 0.4s ease-in;
-}
-
-.btn:hover > svg {
-  font-size: 1.2em;
-  transform: translateX(-5px);
 }
 
 .btn:hover {
