ce4fd71dbce88c60223ea3740237b41481dd0da6
EternalHeart <hao131462@qq.com>
2026-09-15 16:22:43 +0800

    fix: 更新 GitHub 仓库地址

--- src/views/NavButton/GitHubButton.vue
+++ src/views/NavButton/GitHubButton.vue
@@ -2,4 +2,4 @@
 import GameIcon from '../Component/GameIcon.vue'
 
 </script>
-<template><a class="icon-button" aria-label="GitHub" title="GitHub" href="https://github.com/SmallTeddy/e-card" target="_blank" rel="noopener noreferrer"><GameIcon name="github" /></a></template>
+<template><a class="icon-button" aria-label="GitHub" title="GitHub" href="https://github.com/SmallTeddyGames/e-card" target="_blank" rel="noopener noreferrer"><GameIcon name="github" /></a></template>
